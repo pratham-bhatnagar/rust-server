b1ace93673067e6b6b42e2c4a4cdf9df794f1c30
Pratham Bhatnagar <prathambhatnagar2803@gmai.com>
2023-01-05 02:53:50 +0530

    Add Html to response

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
+use std::fs;
 use std::net::{TcpListener,TcpStream};
+use std::io::prelude::*;
 fn main() {
   let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
 
   for stream in listner.incoming(){
     let stream = stream.unwrap();
-    println!("Connection Established!")
+  handle_connection(stream);
   }
+}
+
+fn handle_connection(mut stream:TcpStream) {
+let mut buffer = [0;1024];
+stream.read(&mut buffer).unwrap();
+let content = fs::read_to_string("index.html").unwrap();
+
+let response = format!("HTTP/1.1 200 OK\r\nContent-Lenght: {}\r\n\r\n{}",content.len(),content);
+stream.write(response.as_bytes()).unwrap();
+stream.flush().unwrap();
 }
